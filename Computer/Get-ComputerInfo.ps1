f8927d5721666a2aeac5b0ea945673e21049afbe
Floris van Enter <floris@vanenter.nl>
2023-01-30 22:03:14 +0100

    Get computer info of 1 or more computers

---
+++ Computer/Get-ComputerInfo.ps1
@@ -0,0 +1,49 @@
+<#
+    .SYNOPSIS
+        Get information on computers
+    .DESCRIPTION
+        Use the CIM to retrieve information from specified computers
+    .PARAMETER ComputerName
+        Which computers, it can be an array
+#>
+
+Param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string[]] $ComputerName
+
+)
+
+Process {
+
+    $Collection = @()
+
+    ForEach ($Computer in $ComputerName) {
+
+        if (Test-Connection -TargetName $Computer) {
+            $Collection += [PSCustomObject]@{
+                Name        = $Computer
+                Online      = $true
+                BIOS        = Invoke-Command -ComputerName $Computer -ScriptBlock { uGet-CimInstance -Query "SELECT * FROM Win32_BIOS" | Select-Object -ExpandProperty Name }
+                OSName      = Invoke-Command -ComputerName $Computer -ScriptBlock { Get-CimInstance -Query 'SELECT * FROM Win32_OperatingSystem' | Select-Object -ExpandProperty Caption }
+                OSVersion   = Invoke-Command -ComputerName $Computer -ScriptBlock { Get-CimInstance -Query 'SELECT * FROM Win32_OperatingSystem' | Select-Object -ExpandProperty Version }
+                InstallDate = Invoke-Command -ComputerName $Computer -ScriptBlock { Get-CimInstance -Query 'SELECT * FROM Win32_OperatingSystem' | Select-Object -ExpandProperty InstallDate }
+                IpAddresses = (Invoke-Command -ComputerName $Computer -ScriptBlock { Get-NetIpAddress | Where-Object IPv4Address -NE $Null } | Select-Object -ExpandProperty IPv4Address) -Join ','
+                Memory      = Invoke-Command -ComputerName $Computer -ScriptBlock { (Get-CimInstance -Class 'CIM_PhysicalMemory' | Select-Object Capacity) / 1Gb }
+            }
+
+        } else {
+            $Collection += [PSCustomObject]@{
+                Name        = $Computer
+                Online      = $false
+                BIOS        = ''
+                OSName      = ''
+                OSVersion   = ''
+                InstallDate = ''
+                IpAddress   = ''
+                Memory      = ''
+            }
+        }
+    }
+
+}
