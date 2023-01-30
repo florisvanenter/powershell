b18f6304283e80161768902c9c5495a2bea139fc
Floris van Enter <Floris@vanEnter.nl>
2023-01-30 14:28:14 +0100

    Add two AD scripts

---
+++ Active Directory/New-AdGroupAndMembers.ps1
@@ -0,0 +1,35 @@
+<#
+    .SYNOPSIS
+        Create a group and add users in an OU
+    .DESCRIPTION
+        Create a group and check users in an OU and add them to a group
+    .PARAMETER Ou
+        The OU where the users are located
+    .PARAMETER GroupName
+        The name of the group to be created
+    .PARAMETER GroupOu
+        The Ou where to put the newly created group
+#>
+
+Param (
+    
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Ou,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $GroupName,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $GroupOu
+
+)
+
+Process {
+
+    New-ADGroup -Name $GroupName -SamAccountName $GroupName -GroupCategory Security -GroupScope Global -Path $GroupOu
+    Get-Aduser -filter * -property Name,Department,DistinguishedName -SearchBase $Ou | ForEach-Object { Add-ADGroupMember -Identity $GroupName -Members $_.SamAccountName }
+
+}
