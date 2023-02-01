fe4d64aa10cd845e9c40e9815073fac6b0afd0e2
Floris van Enter <floris@vanenter.nl>
2023-02-01 22:02:04 +0100

    Change UserPrincipalName in AD

---
+++ Active Directory/Update-UserprincipalName.ps1
@@ -0,0 +1,42 @@
+<#
+    .SYNOPSIS
+        Update an UPN with a new one
+    .DESCRIPTION
+        Fliter on an OU with the old UPN and update with a new one
+    .PARAMETER SearchBase
+        The Ou to search objects in, empty or null means in complete AD
+    .PARAMETER OldUpn
+        The UPN to replace
+    .PARAMETER NewUpn
+        The UPN to replace it with
+#>
+
+Param (
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $SearchBase,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $OldUpn,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $NewUpn
+
+)
+
+Process {
+
+    If (SearchBase) {
+        $Targets = Get-ADUser -Filter { UserPrincipalName -like "*$OldUpn" } -SearchBase $SearchBase -Properties UserPrincipalName
+    } Else {
+        $Targets = Get-ADUser -Filter { UserPrincipalName -like "*$OldUpn" } -Properties UserPrincipalName
+    }
+
+    $Targets | ForEach-Object { Set-ADUser -Identity $_.SamAccountName -UserPrincipalName ( $_.UserPrincipalName.Replace(('@' + $OldUpn), ('@' + $NewUpn)) ) }
+
+}
+
+
